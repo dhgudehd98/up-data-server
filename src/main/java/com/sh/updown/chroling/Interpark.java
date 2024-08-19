cf314a5274846a62cccf9118963d70840ecc0e0b
dhgudehd98 <ohd7150@nate.com>
2024-08-19 17:44:00 +0900

    fix : 서버 환경에서 chrome driver 구동할 수 있도록 변경

--- src/main/java/com/sh/updown/chroling/Interpark.java
+++ src/main/java/com/sh/updown/chroling/Interpark.java
@@ -34,7 +34,7 @@ public class Interpark {
 
         //ChromeDriver 옵션 설정 및 연결
         //로컬 환경에서의 Chromedriver 실행
-//        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver")
 
         //서버에서 chromedriver 구축
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
