c12182505439fb5e4fd3e82202274e8ab568fe9b
dhgudehd98 <ohd7150@nate.com>
2024-08-19 20:05:33 +0900

    fix : 서버 환경에서 chrome driver 구동할 수 있도록 변경

--- start.sh
+++ start.sh
@@ -12,7 +12,8 @@ yum install -y wget unzip
 # Chrome Headless Shell 다운로드 및 설치
 yum install -y dbus-libs
 echo "Downloading and installing Chrome Headless Shell..."
-wget https://storage.googleapis.com/chrome-for-testing-public/127.0.6533.119/linux64/chrome-headless-shell-linux64.zip
+#wget https://storage.googleapis.com/chrome-for-testing-public/127.0.6533.119/linux64/chrome-headless-shell-linux64.zip
+wget https://storage.googleapis.com/chrome-for-testing-public/127.0.6533.119/linux64/chrome-linux64.zip
 unzip chrome-headless-shell-linux64.zip -d /usr/local/bin/
 
 # 설치 완료 알림
