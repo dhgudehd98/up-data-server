6a7c37ff6a74d8fdf7e21e5b652a6eac1d78241c
dhgudehd98 <ohd7150@nate.com>
2024-08-19 11:47:35 +0900

    fix : 서버 환경에서 chrome driver 구동할 수 있도록 변경

--- src/main/java/com/sh/updown/chroling/Interpark.java
+++ src/main/java/com/sh/updown/chroling/Interpark.java
@@ -38,6 +38,7 @@ public class Interpark {
 
         //서버에서 chromedriver 구축
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        System.out.println("ChromeDriver path set to: " + System.getProperty("webdriver.chrome.driver"));
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--headless"); // 브라우저 UI를 표시하지 않음 (headless 모드)
