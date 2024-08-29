ad11a5ae29df2c10a028bd66064d88fc538b4aee
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2024-08-30 00:53:51 +0900

    Refactoring : Interpark 코드 수정 및 불필요 코드 제거

--- src/main/java/com/up/down/batch/crawling/Interpark.java
+++ src/main/java/com/up/down/batch/crawling/Interpark.java
@@ -42,12 +42,12 @@ public class Interpark {
 
         //ChromeDriver 옵션 설정 및 연결
         //로컬 환경에서의 Chromedriver 실행
-        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
 
         //서버에서 chromedriver 구축
         ChromeOptions options = new ChromeOptions();
-//        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-//        options.setBinary("/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        options.setBinary("/usr/bin/google-chrome");
         options.addArguments("--headless");
         options.addArguments("--no-sandbox"); // 추가한 옵션
         options.addArguments("--disable-dev-shm-usage");
